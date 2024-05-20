7a735a3680076d5986c3d2731c703a5b458c7dd8
Callum Everatt <159017285+cal1201@users.noreply.github.com>
2024-05-20 10:06:36 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,38 +7,24 @@ data "aws_iam_policy" "cloudwatch_readonly" {
 }
 
 module "lambda" {
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-lambda?ref=v6.0.0"
+  source = "git::https://github.com/FinLegal/terraform-aws-lambda.git?ref=3c26409f2d0131986e04cdfb787ed50e8ef96ad3"
 
-  function_name            = var.name
-  description              = "Post messages from AWS to Slack"
-  handler                  = "aws-to-slack/src/index.handler"
-  artifacts_dir            = var.lambda.artifacts_dir
-  build_in_docker          = var.lambda.build_in_docker
-  create_package           = var.lambda.create_package
-  ephemeral_storage_size   = var.lambda.ephemeral_storage_size
-  ignore_source_code_hash  = var.lambda.ignore_source_code_hash
-  local_existing_package   = var.lambda.local_existing_package
-  memory_size              = var.lambda.memory_size
-  recreate_missing_package = var.lambda.recreate_missing_package
-  runtime                  = var.lambda.runtime
-  s3_bucket                = var.lambda.s3_bucket
-  s3_existing_package      = var.lambda.s3_existing_package
-  s3_prefix                = var.lambda.s3_prefix
-  store_on_s3              = var.lambda.store_on_s3
-  timeout                  = var.lambda.timeout
+  function_name = var.name
+  description   = "Post messages from AWS to Slack"
+  handler       = "aws-to-slack/src/index.handler"
+  runtime       = "nodejs14.x"
+  timeout       = 10
 
-  source_path = [
-    {
-      path             = "${path.module}/vendor"
-      pip_requirements = true
-      patterns         = ["!\\.terragrunt-source-manifest"]
+  source_path = "${path.module}/vendor"
+
+  policy = {
+    json = data.aws_iam_policy.cloudwatch_readonly.policy
+  }
+
+  environment = {
+    variables = {
+      SLACK_HOOK_URL = var.hook_url
     }
-  ]
-
-  policy = data.aws_iam_policy.cloudwatch_readonly.policy
-
-  environment_variables = {
-    SLACK_HOOK_URL = var.hook_url
   }
 }
 
@@ -62,14 +48,14 @@ resource "aws_cloudwatch_event_target" "this" {
   count = length(var.event_rules)
 
   rule = aws_cloudwatch_event_rule.this[count.index].name
-  arn  = module.lambda.lambda_function_arn
+  arn  = module.lambda.function_arn
 }
 
 resource "aws_lambda_permission" "lambda" {
   count = length(var.event_rules)
 
   action        = "lambda:InvokeFunction"
-  function_name = module.lambda.lambda_function_name
+  function_name = module.lambda.function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.this[count.index].arn
 }
@@ -85,14 +71,14 @@ resource "aws_sns_topic_subscription" "this" {
 
   topic_arn = each.value
   protocol  = "lambda"
-  endpoint  = module.lambda.lambda_function_arn
+  endpoint  = module.lambda.function_arn
 }
 
 resource "aws_lambda_permission" "sns" {
   for_each = var.create_sns_topic ? { "arn" : aws_sns_topic.this[0].arn } : { for arn in var.sns_topics : arn => arn }
 
   action        = "lambda:InvokeFunction"
-  function_name = module.lambda.lambda_function_name
+  function_name = module.lambda.function_name
   principal     = "sns.amazonaws.com"
   source_arn    = each.value
 }
